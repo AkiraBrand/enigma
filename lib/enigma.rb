aa3c2967bd2f163af443193574e0f6c3e2dd51ee
Averi Johnson <averimj@live.com>
2018-08-25 16:54:28 -0600

    add key class and some functionality

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,13 @@
 require 'date'
 
 class Enigma
-  attr_reader :key
 
-  def initialize(message, key = "54321", date = Date.today)
-    @message = message
-    @key = key
-    @date = date
+
+  def initialize
+
   end
 
+  def encrypt (message, key = Key.new, date = Date.today)
+
+  end
 end
--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,11 @@
 class Key
+  attr_reader :number
 
-  def initialize(message)
-    @message = message
+  def initialize(number)
+    @number = number
+  end
+
+  def random_number
+    rand(00000..99999).to_s
   end
 end
