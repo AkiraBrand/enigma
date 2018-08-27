32cfb682b819f0023786ee167ba63ed2a21a6d18
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-27 10:04:04 -0600

    adds beginnings of date formatter and also got the test helper to work

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,19 +1,19 @@
-require 'date'
+require 'Date'
 
 class Enigma
 
-  attr_reader :character_map,
-              :split_keys
+attr_reader :character_map,
+            :date
+
 
   def initialize
-   @character_map = [*('a'..'z'), *('0'..'9'), ' ', '.', ',']
-   @split_keys = []
+    @character_map = [*('a'..'z'), *('0'..'9'), ' ', '.',',']
+    @split_keys = []
   end
 
-  def encrypt (message, key = key_generator, date = Date.today)
-    @split_keys << key.chars
-
-
+  def encrypt(message, key = key_generator, date = Date.today)
+    @split_keys = key.chars
+    @date = date
   end
 
   def key_generator
@@ -21,11 +21,15 @@ class Enigma
   end
 
   def key_holder
-    held_keys = []
-    held_keys << @split_keys.join[0..1]
-    held_keys << @split_keys.join[1..2]
-    held_keys << @split_keys.join[2..3]
-    held_keys << @split_keys.join[3..4]
+    @held_keys = []
+    @held_keys << @split_keys.join[0..1]
+    @held_keys << @split_keys.join[1..2]
+    @held_keys << @split_keys.join[2..3]
+    @held_keys << @split_keys.join[3..4]
+  end
+
+  def format_random_date
+
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,6 @@
-# require 'simplecov'
-# SimpleCov.start
-# require './test/test_helper'
+require 'simplecov'
+SimpleCov.start
+require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
@@ -16,21 +16,40 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_it_starts_with_a_long_character_map
-    expected = [*('a'..'z'), *('0'..'9'), ' ', '.', ',']
+  def test_it_starts_with_a_character_map
+    expected = [*('a'..'z'), *('0'..'9'), ' ', '.',',']
+    #without the splat, it won't actually expand upon what you want
+    #it just returns this: ["a".."z", "0".."9", " ", ".", ","]
+    #like....thanks bro
     assert_equal expected, @enigma.character_map
   end
 
-  def test_the_random_key_generator_padding_works
+  def test_the_random_key_generator_generates_a_five_character_key
     assert_equal 5, @enigma.key_generator.length
-      binding.pry
+    #I tested this a few times without rjust, and it spat out a 5 character
+    #string each time. i will put the rjust back in, just to be safe
+    #but i'm wondering if its absolutely necessary
   end
 
-  def test_the_key_holder_holds_a_given_key
+  def test_the_master_key_holder_with_given_key
     @enigma.encrypt("hello", "12345")
-    expected = ["12", "23", "34", "45"]
-    assert_equal expected, @enigma.key_holder
+    assert_equal ["12", "23", "34", "45"], @enigma.key_holder
   end
 
+  def test_random_date_generator_returns_only_the_year
+    @enigma.encrypt("hello")
+     binding.pry
+    assert_equal 18, @enigma.format_random_date
+
+  end
 
 end
+
+
+# The date of message transmission is also factored into the encryption
+# Consider the date in the format DDMMYY, like 240818
+# Square the numeric form (412699225) and find the last four digits (9225)
+# The first digit is the “A offset” (9)
+# The second digit is the “B offset” (2)
+# The third digit is the “C offset” (2)
+# The fourth digit is the “D offset” (5)
